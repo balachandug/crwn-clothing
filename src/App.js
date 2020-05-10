1367fe9beb55bace2e61f629805e32bc43e27972
Balu <balachandu.gillella@sap.com>
2020-05-10 19:25:04 +0530

    added header component to our app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './components/homepage/homepage.component'
 import ShopPage from './components/shop/shop.component';
 import './App.css';
 
+import Header from './components/header/header.component'
+
 
 function App() {
   return (
       <div>
-        <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop' component={ShopPage}/>
+          <Header/>
+          <Switch>
+              <Route exact path='/' component={HomePage}/>
+              <Route exact path='/shop' component={ShopPage}/>
+          </Switch>
       </div>
   );
 }
