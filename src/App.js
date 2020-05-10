f6d558e024a8ab2d4646416ab82c9e9fe81db4be
Balu <balachandu.gillella@sap.com>
2020-05-10 19:00:11 +0530

    Updating our app with Shopping Page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import HomePage from './components/homepage/homepage.component'
+import ShopPage from './components/shop/shop.component';
 import './App.css';
 
-const HatsPage = () => (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-);
 
 function App() {
   return (
       <div>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/hats' component={HatsPage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </div>
   );
 }
